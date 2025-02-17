4be53e379da21e7e182bd81a7997207b1185b93a
Sungmook Lim <cygnusx_1@naver.com>
2025-02-17 23:18:40 +0900

    test_function2 was added

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import numpy as np
 def test_function():
     print('Test function2')
     
+def test_function2():
+    print('Second test function')
+
     
 if __name__ == '__main__':
     test_function()
